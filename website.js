a691184e72509bb786d9ed2057c6bd12a4fc7dec
MichaelASC6 <michaelquin129@gmail.com>
2019-08-14 15:28:41 -0400

    slideShow commit

--- website.js
+++ website.js
@@ -216,3 +216,20 @@ let codeCountries = ["pediatrician", "dentist", "sport-physical-therapist", "spo
 
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), countries);
+
+
+// Our slide show starts here and not anywhere else
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+ var i;
+ var slides = document.getElementsByClassName("mySlides");
+ for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";
+ }
+ slideIndex++;
+ if (slideIndex > slides.length) {slideIndex = 1}
+ slides[slideIndex-1].style.display = "block";
+ setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
